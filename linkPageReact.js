c3d1cdbcdd368dc20884854d167bd48e1441c1e4
Dev Dave <115370760+badshah5464@users.noreply.github.com>
2023-04-01 01:52:06 +0530

    Add React

--- linkPageReact.js
+++ linkPageReact.js
@@ -1,12 +1,9 @@
+const reactPageLink = [{
+    link: "./React-App-Install-01.html",
+    pageName: "React App Install"
+}, ];
+
 let reactLink = document.getElementById("reactLink");
-
-const reactPageLink = [
-    {
-        link: "./React-introduction-01.html",
-        pageName: "React App Install Method"
-    },
-];
-
 reactPageLink.map(function (x) {
     reactLink.innerHTML += `<li><a href="${x.link}">${x.pageName}</a></li>`
 })
--- search.js
+++ search.js
@@ -1,3 +1,26 @@
-const searchIcon=document.getElementById("searchIcon"),searchInput=document.getElementById("searchInput"),searchIconChange=document.getElementById("searchIconChange1;");searchIcon.addEventListener("click",(function(){"searchInput"===searchInput.className?searchInput.className+=" searchResponse":searchInput.className="searchInput"}));
 //! Search Input 
-const addMenu=document.getElementById("addMenu"),searchValue=[{link:"./HTML-Home-01.html",pageName:"HTML Basic"},{link:"./html-introduction-02.html",pageName:"HTML introduction"},{link:"./html-Editor-03.html",pageName:"HTML Editor"},{link:"./CSS-Basic-01.html",pageName:"CSS Tutorial"},{link:"./JavaScript-Basic-01.html",pageName:"Basic of JavaScript"},{link:"./AdvanceMethod.html",pageName:"Advance Method's"},{link:"./React-Introduction-01.html",pageName:"ReactJs Introduction"}];function searchFunction(){let e,a,n,t,c,s;for(e=document.getElementById("search"),a=e.value.toUpperCase(),n=document.getElementsByClassName("searchMenu"),t=document.getElementsByClassName("searchList"),s=0;s<t.length;s++)c=t[s].getElementsByClassName("item")[0],c.innerHTML.toUpperCase().indexOf(a)>-1?t[s].style.display="":t[s].style.display="none"}searchValue.map((function(e){addMenu.innerHTML+=`\n        <li class="searchList"><a href="${e.link}" class="item">${e.pageName}</a></li>\n    `}));
+const addMenu = document.getElementById("addMenu"),
+    searchValue = [{
+        link: "./HTML-Home-01.html",
+        pageName: "HTML Basic"
+    }, {
+        link: "./html-introduction-02.html",
+        pageName: "HTML introduction"
+    }, {
+        link: "./html-Editor-03.html",
+        pageName: "HTML Editor"
+    }, {
+        link: "./CSS-Basic-01.html",
+        pageName: "CSS Tutorial"
+    }, {
+        link: "./JavaScript-Basic-01.html",
+        pageName: "Basic of JavaScript"
+    }, {
+        link: "./AdvanceMethod.html",
+        pageName: "All Method's"
+    }, {
+        link: "./React-Introduction-01.html",
+        pageName: "React App Install"
+    }];
+
+const searchIcon=document.getElementById("searchIcon"),searchInput=document.getElementById("searchInput"),searchIconChange=document.getElementById("searchIconChange1;");function searchFunction(){let e,s,a,n,t,c;for(e=document.getElementById("search"),s=e.value.toUpperCase(),a=document.getElementsByClassName("searchMenu"),n=document.getElementsByClassName("searchList"),c=0;c<n.length;c++)t=n[c].getElementsByClassName("item")[0],t.innerHTML.toUpperCase().indexOf(s)>-1?n[c].style.display="":n[c].style.display="none"}searchIcon.addEventListener("click",(()=>{"searchInput"===searchInput.className?searchInput.className+=" searchResponse":searchInput.className="searchInput"})),searchValue.map((function(e){addMenu.innerHTML+=`<li class="searchList"><a href="${e.link}" class="item">${e.pageName}</a></li>`}));
